66e40b76d644daa6e954e4c237c243a9fda673d0
Sorakrit05 <aomsin22122558@gmail.com>
2024-12-28 15:03:54 +0700

    HYN

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,41 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    
+    double sumA = 0;
+    for(int i=0;i<N;i++){
+        sumA += A[i];
+    }
+    B[0] = sumA/N;
+    
+    double sumS = 0;
+    for(int i=0;i<N;i++){
+        sumS += pow((A[i]-B[0]),2);
+    }
+    B[1] = sqrt(sumS/N);
+    
+    double sumG=1;
+    for(int i=0;i<N;i++){
+        sumG *= A[i];
+    }
+    B[2] = pow(sumG,(1.0/N));
+    
+    double sumH = 0;
+    for(int i=0;i<N;i++){
+        sumH += 1.0/A[i];
+    }
+    B[3] = N/sumH;
+    
+    double max = A[0];
+    double min = A[0];
+    for(int i=0;i<N;i++){
+        if(A[i]>max) max =A[i];
+        if(A[i]<min) min =A[i];
+    }
+    B[4]=max;
+    B[5]=min;
+        
+}
+
